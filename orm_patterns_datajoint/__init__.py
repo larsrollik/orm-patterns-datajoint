949ac2bf3d642f2e2610c2bc892432e7a7aa37c9
Lars B. Rollik <L.B.Rollik@protonmail.com>
2025-05-26 14:51:13 +0100

    extended config to hold global-schema-prefix as extra field

--- orm_patterns_datajoint/__init__.py
+++ orm_patterns_datajoint/__init__.py
@@ -21,10 +21,6 @@ def prepare_schema_name(file):
     return Path(file).name.replace(".py", "")
 
 
-def get_global_schema_prefix():
-    return "EXAMPLE_GLOBAL_PREFIX"
-
-
 def get_datajoint_schema(
     schema_prefix=None,
     schema_name=None,
@@ -36,7 +32,7 @@ def get_datajoint_schema(
 
     """
     if schema_prefix is None:
-        schema_prefix = get_global_schema_prefix()
+        schema_prefix = dj.config.get("global_schema_prefix", "GLOBAL_SCHEMA_PREFIX_DEFAULT")
 
     schema = dj.Schema(
         schema_name="__".join([schema_prefix, schema_name]),
